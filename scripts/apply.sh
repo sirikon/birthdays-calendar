9f026d98760ff52f8770f7338ca258c029ef40d3
Carlos Fernández Llamas <hello@sirikon.me>
2019-03-16 12:30:36 +0100

    Add kubernetes definitions

---
+++ scripts/apply.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+BASH_RELATIVE_PATH="${BASH_SOURCE%/*}"
+cd "${BASH_RELATIVE_PATH}/../kubernetes/out"
+
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
---
+++ scripts/gen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+BASH_RELATIVE_PATH="${BASH_SOURCE%/*}"
+cd "${BASH_RELATIVE_PATH}/../kubernetes"
+
+mkdir ./out
+
+cat deployment.yaml | envsubst > ./out/deployment.yaml
+cat service.yaml | envsubst > ./out/service.yaml
